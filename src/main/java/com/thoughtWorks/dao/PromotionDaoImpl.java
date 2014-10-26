379a77cf709437ce1ad422c93885f4c5b09a1116
zzh <xiaoyanzhuzzh@sina.com>
2014-10-26 15:39:19 +0800

    implement functions

---
+++ src/main/java/com/thoughtWorks/dao/PromotionDaoImpl.java
@@ -0,0 +1,32 @@
+package com.thoughtWorks.dao;
+
+import com.thoughtWorks.vo.Promotion;
+
+import java.util.List;
+
+public class PromotionDaoImpl implements PromotionDao {
+    @Override
+    public Promotion getPromotionById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Promotion> getPromotions() {
+        return null;
+    }
+
+    @Override
+    public void insertPromotion(Promotion promotion) {
+
+    }
+
+    @Override
+    public void deletePromotionById(int id) {
+
+    }
+
+    @Override
+    public void updatePromtion(Promotion promotion) {
+
+    }
+}
