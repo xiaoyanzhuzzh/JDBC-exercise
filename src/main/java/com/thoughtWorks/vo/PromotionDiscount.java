55306be4711f4e6da14d66d54edb0f0a5cf5d360
zzh <xiaoyanzhuzzh@sina.com>
2014-11-05 18:52:59 +0800

    create implement and class

---
+++ src/main/java/com/thoughtWorks/vo/PromotionDiscount.java
@@ -0,0 +1,11 @@
+package com.thoughtWorks.vo;
+
+/**
+ * Created by zhangzhihui on 14-10-26.
+ */
+public class PromotionDiscount extends Promotion {
+    @Override
+    public double calculation(Item item, double number) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/thoughtWorks/vo/PromotionFactory.java
@@ -0,0 +1,4 @@
+package com.thoughtWorks.vo;
+
+public class PromotionFactory {
+}
